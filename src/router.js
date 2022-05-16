4a66371763d3991cb36edd7b56918fbcaeba449b
Victor <ajom9@hotmail.com>
2022-05-16 17:25:56 -0500

    Primer commit del proyecto de criptomonedas

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home";
+import About from "@/views/AboutPage";
+import Error from "@/views/ErrorPage";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+    {
+      path: "*",
+      name: "error",
+      component: Error,
+    },
+  ],
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <px-assets-table />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+export default {
+  name: "HomePage",
+  components: { PxAssetsTable },
+};
+</script>
